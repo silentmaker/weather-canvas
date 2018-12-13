d8d2fcf782c4ef946561261a4b7d90ad694856a3
silentmaker <demonmaxc@gmail.com>
2018-12-14 03:16:24 +0800

    Updates

---
+++ precache-manifest.86a3d1bbd4101c57c3865c2b1a694914.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f3b2f7c461957c283fa",
+    "url": "/weather-canvas/css/app.9750995f.css"
+  },
+  {
+    "revision": "2f3b2f7c461957c283fa",
+    "url": "/weather-canvas/js/app.8b2d9832.js"
+  },
+  {
+    "revision": "ba011c2c887aadbefbe5",
+    "url": "/weather-canvas/js/chunk-vendors.6b8ae08f.js"
+  },
+  {
+    "revision": "5a40befbb3f2c2f7130d81e3e42eb823",
+    "url": "/weather-canvas/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/weather-canvas/robots.txt"
+  }
+];
